bbb79c43a1db6348ce7fbdfcc53b498e2b712eaf
Frederik Heda <frederik.heda@gmx.net>
2021-04-10 00:25:55 +0100

    structured library import

--- Main.py
+++ Main.py
@@ -8,7 +8,6 @@ from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import layers
-
 import matplotlib.pyplot as plt
 
 #Assign Directories

c51b523a7ce219ab357f5f467bc92d7bab270df2
Frederik Heda <frederik.heda@gmx.net>
2021-04-09 23:17:09 +0100

    Add libraries

--- Main.py
+++ Main.py
@@ -1,3 +1,13 @@
 #Covid-19 vs Viral Pneumonia vs Health Lung classifier for X-Ray images
 #By Frederik Heda
 #09/04/2021
+
+#import libraries
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers
+
+import matplotlib.pyplot as plt
+

9f6f54794778ab9277f1b4370bbda58e6aa31bac
Frederik Heda <frederik.heda@gmx.net>
2021-04-12 00:14:51 +0100

    modification of hyperparameters

--- Main.py
+++ Main.py
@@ -14,7 +14,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-from tensorflow.python.keras.layers.core import Activation
+#from tensorflow.python.keras.layers.core import Activation
 
 #Assign Directories
 DIRECTORY_train = "Covid_Data/Covid19-dataset/train"
@@ -23,7 +23,7 @@ DIRECTORY_predict = "Covid_Data/Covid19-dataset/Predict"
 CLASS_MODE = "categorical"
 COLOR_MODE = "grayscale"
 TARGET_SIZE = (256,256)
-BATCH_SIZE = 4
+BATCH_SIZE = 16
 
 #Creating Training Data
 training_data_generator = ImageDataGenerator(1.0/255, zoom_range = 0.25, rotation_range = 20, width_shift_range = 0.05, height_shift_range = 0.05)
@@ -33,11 +33,11 @@ training_iterator = training_data_generator.flow_from_directory(DIRECTORY_train,
 training_iterator.next()
 
 #Create Validation Data
-validation_data_generator = ImageDataGenerator(rescale = 1./255)
+validation_data_generator = ImageDataGenerator()
 validation_iterator = validation_data_generator.flow_from_directory(DIRECTORY_test,class_mode=CLASS_MODE,color_mode=COLOR_MODE,batch_size=BATCH_SIZE)
 
 #Create Prediction Data
-prediction_data_generator = ImageDataGenerator(rescale = 1./255)
+prediction_data_generator = ImageDataGenerator()
 prediction_iterator = validation_data_generator.flow_from_directory(DIRECTORY_predict,class_mode=CLASS_MODE,color_mode=COLOR_MODE,batch_size=1)
 prediction_iterator.next()
 
@@ -46,8 +46,8 @@ model = Sequential(name = "Covid")
 #Input Layer
 model.add(layers.Input(shape=(256,256,1)))
 #Add Hidden Layers
-#model.add(layers.Conv2D(8,8, strides = 3, activation = 'relu'))
-#model.add(layers.MaxPooling2D(pool_size = (5,5), strides = (3,3)))
+model.add(layers.Conv2D(8,8, strides = 3, activation = 'relu'))
+model.add(layers.MaxPooling2D(pool_size = (2,2), strides = (2,2)))
 model.add(layers.Conv2D(5,5, strides = 2, activation = 'relu'))
 model.add(layers.MaxPooling2D(pool_size = (2,2), strides = (2,2)))
 model.add(layers.Conv2D(3,3, strides = 1, activation = 'relu'))
@@ -55,13 +55,11 @@ model.add(layers.MaxPooling2D(pool_size = (2,2), strides = (2,2)))
 
 #Flatten Image:
 model.add(layers.Flatten())
-#Dense Layer
-model.add(layers.Dense(5,activation="relu"))
 #Output Layer
 model.add(layers.Dense(3,activation = 'softmax'))
 
 #Define Hyperparameters
-optimizer = tf.keras.optimizers.Adam(learning_rate = 0.0001)
+optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
 loss_function = tf.keras.losses.CategoricalCrossentropy()
 metrics_function=[tf.keras.metrics.CategoricalAccuracy(),tf.keras.metrics.AUC()]
 
@@ -71,7 +69,7 @@ model.compile(optimizer = optimizer, loss = loss_function, metrics = metrics_fun
 #Print Model Summary
 #print(model.summary())
 
-model.fit(training_iterator, steps_per_epoch = training_iterator.samples/BATCH_SIZE, epochs = 15, validation_data = validation_iterator, validation_steps = validation_iterator.samples/BATCH_SIZE)
+model.fit(training_iterator, steps_per_epoch = training_iterator.samples/BATCH_SIZE, epochs = 20, validation_data = validation_iterator, validation_steps = validation_iterator.samples/BATCH_SIZE)
 
 predictions = model.predict(prediction_iterator, verbose = 1)
 predicted_classes = np.argmax(predictions, axis=1)

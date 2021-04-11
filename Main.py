638ffa4d752b3cede5431d26b41f6e4a3af0b3fc
Frederik Heda <frederik.heda@gmx.net>
2021-04-12 00:51:47 +0100

    model tuning

--- Main.py
+++ Main.py
@@ -26,18 +26,18 @@ TARGET_SIZE = (256,256)
 BATCH_SIZE = 16
 
 #Creating Training Data
-training_data_generator = ImageDataGenerator(1.0/255, zoom_range = 0.25, rotation_range = 20, width_shift_range = 0.05, height_shift_range = 0.05)
+training_data_generator = ImageDataGenerator(rescale=1.0/255, zoom_range = 0.25, rotation_range = 20, width_shift_range = 0.05, height_shift_range = 0.05)
 #training_data_generator = ImageDataGenerator(rescale = 1./255)
 training_iterator = training_data_generator.flow_from_directory(DIRECTORY_train,class_mode=CLASS_MODE,color_mode=COLOR_MODE,batch_size=BATCH_SIZE)
 
 training_iterator.next()
 
 #Create Validation Data
-validation_data_generator = ImageDataGenerator()
+validation_data_generator = ImageDataGenerator(rescale=1.0/255)
 validation_iterator = validation_data_generator.flow_from_directory(DIRECTORY_test,class_mode=CLASS_MODE,color_mode=COLOR_MODE,batch_size=BATCH_SIZE)
 
 #Create Prediction Data
-prediction_data_generator = ImageDataGenerator()
+prediction_data_generator = ImageDataGenerator(rescale=1.0/255)
 prediction_iterator = validation_data_generator.flow_from_directory(DIRECTORY_predict,class_mode=CLASS_MODE,color_mode=COLOR_MODE,batch_size=1)
 prediction_iterator.next()
 
@@ -46,15 +46,17 @@ model = Sequential(name = "Covid")
 #Input Layer
 model.add(layers.Input(shape=(256,256,1)))
 #Add Hidden Layers
-model.add(layers.Conv2D(8,8, strides = 3, activation = 'relu'))
-model.add(layers.MaxPooling2D(pool_size = (2,2), strides = (2,2)))
-model.add(layers.Conv2D(5,5, strides = 2, activation = 'relu'))
-model.add(layers.MaxPooling2D(pool_size = (2,2), strides = (2,2)))
-model.add(layers.Conv2D(3,3, strides = 1, activation = 'relu'))
-model.add(layers.MaxPooling2D(pool_size = (2,2), strides = (2,2)))
+model.add(layers.Conv2D(5,5, strides = 2,padding="same", activation = 'relu'))
+model.add(layers.MaxPooling2D(pool_size = (2,2), strides = (2,2), padding="valid"))
+model.add(layers.Conv2D(3,3, strides = 1, padding="same", activation = 'relu'))
+model.add(layers.MaxPooling2D(pool_size = (2,2), strides = (2,2),padding="valid"))
 
 #Flatten Image:
 model.add(layers.Flatten())
+
+#Hidden Layer
+model.add(layers.Dense(8,activation="relu"))
+model.add(layers.Dense(5,activation="relu"))
 #Output Layer
 model.add(layers.Dense(3,activation = 'softmax'))
 
@@ -69,7 +71,7 @@ model.compile(optimizer = optimizer, loss = loss_function, metrics = metrics_fun
 #Print Model Summary
 #print(model.summary())
 
-model.fit(training_iterator, steps_per_epoch = training_iterator.samples/BATCH_SIZE, epochs = 20, validation_data = validation_iterator, validation_steps = validation_iterator.samples/BATCH_SIZE)
+model.fit(training_iterator, steps_per_epoch = training_iterator.samples/BATCH_SIZE, epochs = 25, validation_data = validation_iterator, validation_steps = validation_iterator.samples/BATCH_SIZE)
 
 predictions = model.predict(prediction_iterator, verbose = 1)
 predicted_classes = np.argmax(predictions, axis=1)
